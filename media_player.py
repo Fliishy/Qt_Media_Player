5b4fa2dd557de3fba5ed58a0c982e84ee5cdf6ad
Linux Desktop <haydenguyn@gmail.com>
2023-07-11 15:11:56 -0700

    Cleaned up the comment formatting

--- media_player.py
+++ media_player.py
@@ -21,59 +21,67 @@ class MediaPlayer (QWidget, Ui_Media_Player):
         # self.pb_back.clicked.connect(self.back_button)
         # self.pb_forward.clicked.connect(self.forward_button)
 
-    # method that runs on startup and allows the user to choose an audio file
+    
+    '''
+    Method that runs on startup and allows the user to choose an audio file
+    file_filter lists the file types we want to show to the user
+    file_path opens a window and lets us choose a file
+    file_path returns a tuple with the file path and data types
+    '''
     def select_file(self):
-        # filtering the type of files we want to show to the user
         file_filter = 'Data file (*.mp3 *.wav *.aac *.flac)'
-        # opens a window and lets us choose a file
-        # returns a tuple with the file path and data types
         self.file_path = QFileDialog.getOpenFileName(
-            # shows what text appears at the top of the windw
-            caption='Select a file',
-            # initialises the directory we display
-            dir='Qt/Media_Player/Audio_Files',
-            # uses the filter list from earlier to only show files of a certain type
-            filter=file_filter
+            caption='Select a file', # shows what text appears at the top of the windw
+            dir='Qt/Media_Player/Audio_Files', # initialises the directory we display
+            filter=file_filter # uses the filter list from earlier to only show files of a certain type
             )
-        # converts the file path to a QFileInfo object
-        # we set file path to index 0 so as to only retrieve the path, not the data types
+        '''
+        Converts the file path to a QFileInfo object
+        We set file path to index 0 so as to only retrieve the path, not the data types
+        Gives us the name of the file from the QFileInfo object
+        Sets the song title label to be the file name we just extracted
+        '''
         file_name_info = QFileInfo(self.file_path[0])
-        # gives us the name of the file from the QFileInfo object
         file_name = file_name_info.fileName()
-        # sets the song title label to be the file name we just extracted
         self.lb_song_title.setText(file_name)
 
-    # method that initialises audio
+    '''
+    Method that initialises audio
+    Sets the url to be the selected file path
+    QAudioOutput represents an output channel for audio
+    Sets the QMediaPlayer audio output to be the defined audio output
+    Tells the player the source of the file is the file we selected in the select_file method
+    Sets the initial volume of the audio output
+    '''
     def set_audio(self):
         
-        # sets the url to be the selected file path
         file_url = QUrl.fromLocalFile(self.file_path[0])
-        # represents an output channel for audio
         self.audio_output = QAudioOutput()
-        # sets the QMediaPlayer audio output to be the defined audio output
         self.player.setAudioOutput(self.audio_output)
-        # tells the player the source of the file is the file we selected earlier
         self.player.setSource(file_url)
-        # sets the initial volume of the audio output
         self.audio_output.setVolume(.2)
         self.player.play()
 
-    # when the play/pause button is pressed
+    '''
+    If the player is playing audio it will pause the player
+    If the player is not playing audio it will play the currently active sound file
+    '''
     def play_pause_button(self):
-        # if the player is playing audio it will pause the player
         if self.player.isPlaying() == True:
             self.player.pause()
-        # if the player is not playing audio it will play the currently active sound file
         else:
             self.player.play()
 
+    '''
+    When the stop button is pressed stops playing and resets the play position to the beginning
+    Sets the active track to an index that doesn't exist so it doesn't play anything
+    Sets the song title text to be a dash
+    '''
     def stop_button(self):
-        # stops playing and resets the play position to the beginning
         self.player.stop()
-        # sets the active track to an index that doesn't exist so it doesn't play anything
         self.player.setActiveAudioTrack(-1)
-        # sets the song title text to be a dash
         self.lb_song_title.setText('-')
+
 
 #creates an instance of QApplication and executes the program
 if __name__ == '__main__':
